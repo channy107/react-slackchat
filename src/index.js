b2983807ab5972b37e2ef379b5dcdc4928470fee
user <hunchan12@gmail.com>
2019-05-23 13:59:22 +0900

    Adding Signout, Clear User Action

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { createStore } from 'redux';
 import { Provider, connect } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
-import { setUser } from './actions';
+import { setUser, clearUser } from './actions';
 
 const store = createStore(rootReducer, composeWithDevTools());
 
@@ -27,6 +27,9 @@ class Root extends React.Component {
                 // console.log(user);
                 this.props.setUser(user);
                 this.props.history.push('/');
+            } else {
+                this.props.history.push('/login');
+                this.props.clearUser();
             }
         })
     }
@@ -48,7 +51,7 @@ const mapStateFromProps = state => ({
 
 const RootWithAuth = withRouter(connect(
     mapStateFromProps,
-    { setUser })(Root));
+    { setUser, clearUser })(Root));
 
 ReactDOM.render(
     <Provider store={store}>
